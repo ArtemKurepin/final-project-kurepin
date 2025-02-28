87646d8ab453b93ef1f49c18fa708e72a6690d0c
Артём <artem.kurepin999@gmail.com>
2025-02-28 22:15:29 +0300

    add method openLoginElement

--- src/main/java/Ru/Avito/PageObjects/MainPage.java
+++ src/main/java/Ru/Avito/PageObjects/MainPage.java
@@ -1,6 +1,7 @@
 package Ru.Avito.PageObjects;
 
 import Ru.Avito.Singleton.AvitoSingleton;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,19 +19,27 @@ public class MainPage {
     private final String URL = "https://www.avito.ru/";
 
     @FindBy(className = "styles-module-input-rA1dB")
-    private WebElement input_Element;
+    private WebElement inputElement;
     @FindBy(tagName = "body")
     private WebElement body;
+    @FindBy(xpath = "//a[@data-marker='header/login-button']")
+    private WebElement loginButton;
 
     public MainPage() {
         this.driver = AvitoSingleton.getDriver();
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
-    public void openLoginPage() {
+    public void openHomePage() {
         driver.get(URL);
         wait.until(ExpectedConditions.visibilityOf(body));
     }
 
+    public WebElement openLoginElement() {
+        loginButton.click();
+        WebElement loginObject= driver.findElement(By.xpath("//form[@data-marker='login-form']"));
+        wait.until(ExpectedConditions.visibilityOf(loginObject));
+        return loginObject;
+    }
 
 }
